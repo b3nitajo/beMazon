1bc80f81670b46b952e8edc92b6ae891a9654f92
Benita Jones <47437893+b3nitajo@users.noreply.github.com>
2019-05-20 21:11:12 -0500

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# beMazon
+# **beMazon**
+
+Get your football gear while supplies last, only at beMazon. Helmets, Cleats, Pads and even gloves.
+
+# What is beMazon?
+BeMazon is an server side storefront built using node.js, inquirer and mySQL database. This app will take in orders from customers and deplete stock from the store's inventory.
+
+# How it works?
+
+First, install required dependencies. Run js file and follow prompts. 
+
+Purchase products and get order totals while product's inventory updates accordingly. 
+
+Once a product is out of stock, it will deactivate and become unorderable.
+
+<video width="320" height="240" controls>
+  <source src="recording.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
--- beMazonCustomer.js
+++ beMazonCustomer.js
@@ -1,38 +1,22 @@
-//save the MySQL and Inquirer npm packages in your homework file
-
-// screenshots (and well-written READMEs) are extremely 
-/////important in the context of GitHub, 
-/////this will be part of the grading.
-//must be markdown type file https://guides.github.com/features/mastering-markdown/
-//require MySQL 
-//Inquirer npm packages
-//include screenshots, a gif, and/or a video of the app working 
-//include these screenshots in a `README.md` file.
-///////This includes views of the prompts and the responses after their selection
-
-//1-4 File beMazon.sql is the MySQL Database called `beMazon`.
-// Table inside of beMazon called `products`.
-// The products table have the following columns:
- //item_id, product_name, department_name, price, stock_quantity
-
+//packages required     
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const {printTable} = require('console-table-printer');
 
-// create the connection information for the sql database
+// connection information for the beMazon database
 var db_connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "BJ2019as!!",
+  password: "",
   database: "beMazon_db"
 });
 
 
-// connect to mysql server and sql database
+// connect to mysql server and beMazon database
 db_connection.connect(function(err) {
   if (err) throw err;
-  // Start ordering process to prompt the user
+  // call Start ordering function
   startOrdering();
 });
 
@@ -41,11 +25,11 @@ function showInventory() {
     console.log("Selecting all products in stock...\n");
     db_connection.query("SELECT * FROM products WHERE stock_quantity > 0", function(err, res) {
       if (err) throw err;
-  //    console.log(res);
       printTable(res);    
     });
 }
 
+//start order function
 function startOrdering(){
     inquirer
     .prompt([
@@ -66,21 +50,20 @@ function startOrdering(){
     }); 
 }
 
+//check if user wants to place new order
 function orderMore(){
     inquirer
     .prompt([
       {
-        name: "needMore",
+        name: "newOrder",
         type: "confirm",
         message: "Place a new order?"
       }
     ])
     .then(function(addItems) {
-        //console.log("passedTest1");
-      if(addItems.needMore === true){
+      if(addItems.newOrder === true){
         db_connection.query("SELECT * FROM products WHERE stock_quantity > 0", function(err, res) {
           if (err) throw err;
-          console.log(res);
           printTable(res);  
           updateQty();
         });
@@ -91,6 +74,7 @@ function orderMore(){
     }); 
 }
 
+//when user is done ordering
 function endOrder(){
   db_connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
@@ -99,59 +83,57 @@ function endOrder(){
   }
 )};
 
-//update database w order details
+//update database with order details
 function updateQty(){
-    // query the database for all products
-    db_connection.query("SELECT * FROM products WHERE stock_quantity > 0", function(err, results) {
-        if (err) throw err;
-        // once you have the items, prompt the user for which they'd like to bid on
-        inquirer
-        .prompt([
-        {
-            name: "item",
-            type: "number",
-            message: "What is the ID of the item you want to purchase?"
-        },
-        {
-            name: "quantity",
-            type: "number",
-            message: "How many do you want?"
+  db_connection.query("SELECT * FROM products WHERE stock_quantity > 0", function(err, results) {
+    if (err) throw err;
+    // once you have the products, prompt the user for order
+    inquirer
+    .prompt([
+    {
+      name: "item",
+      type: "number",
+      message: "What is the ID of the item you want to purchase?"
+    },
+    {
+      name: "quantity",
+      type: "number",
+      message: "How many do you want?"
+    }
+    ])
+    .then(function(answer) {
+      // get the information of the chosen item
+      var orderedProduct;
+      var remainQty;   
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].item_id === answer.item) {
+          orderedProduct = answer.item;
+          //check to see if there is stock
+          if(results[i].stock_quantity >= parseInt(answer.quantity)){
+            remainQty = results[i].stock_quantity - parseInt(answer.quantity);
+            var orderTotal = parseInt(answer.quantity) * results[i].price;
+            console.log("Your order total is $" + orderTotal  + ". Thank you for shopping!");
+          }
+          //If cannot fulfill order
+          else{
+            remainQty = results[i].stock_quantity;
+            console.log("Sorry, we can't fill your order, We have " + remainQty + " left.");
+          }
         }
-        ])
-        .then(function(answer) {
-            // get the information of the chosen item
-            var orderedProduct;
-            var remainQty;   
-            for (var i = 0; i < results.length; i++) {
-               if (results[i].item_id === answer.item) {
-                    orderedProduct = answer.item;
-                    //check to see if there is stock
-                    if(results[i].stock_quantity >= parseInt(answer.quantity)){
-                      remainQty = results[i].stock_quantity - parseInt(answer.quantity);
-                      var orderTotal = parseInt(answer.quantity) * results[i].price;
-                      console.log("Your order total is $" + orderTotal  + ". Thank you for shopping!");
-                    }
-                    //If cannot fulfill order
-                    else{
-                        remainQty = results[i].stock_quantity;
-                        console.log("Sorry, we can't fill your order, We have " + remainQty + " left.");
-                    }
-                }
-            }
-            // when product is ordered
-            if (orderedProduct) {      
-                const queryUpdate = "UPDATE products SET stock_quantity = " + remainQty + " WHERE item_id = " + orderedProduct;
-                console.log(queryUpdate);      
-                db_connection.query(queryUpdate, function(err, res) {
-                   if (err) throw err;
-                  // printTable(res);
-                });
-            }
-            else {
-                console.log("We do not have that item. Select Y to try again");
-            }
-            orderMore();
-        });
+      }
+      // when product is ordered
+      if (orderedProduct) {      
+        const queryUpdate = "UPDATE products SET stock_quantity = " + remainQty + " WHERE item_id = " + orderedProduct;
+        console.log(queryUpdate);      
+        db_connection.query(queryUpdate, function(err, res) {
+          if (err) throw err;
+          });
+      }
+      else {
+         console.log("We do not have that item. Select Y to try again");
+      }
+      orderMore();
     });
+  });
 }
  
